201ddf8a2de8c38ef49965b3078bc58e038b90b7
imartemy1524 <41122604+imartemy1524@users.noreply.github.com>
2020-09-12 16:07:37 +0300

    Update vk-audio-url-decoder.cpp

--- vk-audio-url-decoder.cpp
+++ vk-audio-url-decoder.cpp
@@ -1,5 +1,4 @@
 #pragma warning(disable : 4996)
-
 #ifdef _MSC_VER
     //  Microsoft 
     #define EXPORT extern "C" __declspec(dllexport)
@@ -9,69 +8,101 @@
 #else
     //Other
     #define EXPORT
-    #define IMPORT
     #pragma warning Unknown dynamic link import/export semantics.
 
 #endif
-
+#pragma region C_CODE
+#pragma warning(disable : 4996)
 
 #include <iostream>
 #include <string>
-
+#include <math.h>
 #include <vector>
+#include <cstdlib>
 
 #include <sstream>
 
 #include <cstring>
 
 #include <algorithm> 
+
 using namespace std;
+#ifndef stol
+long stol(string& str) {
+    long i;
+    sscanf(str.c_str(), "%d", &i);
+    return i;
+}
+#endif // !std::stol
+struct List {
+    std::stringstream value;
+    List* next;
+};
 const char Qt[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMN0PQRSTUVWXYZO123456789+/=";
 const char Qt2[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMN0PQRSTUVWXYZO123456789+/=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMN0PQRSTUVWXYZO123456789+/=";
-
 long id;
 
+#pragma region not_using_func
+long QTIndex(char i) {
+    const char* ptr = strchr(Qt, i);
+    return ptr ? (ptr - (Qt)) : -1;
+}
+void delete_List(List* start, long i) {
+    List* going;
+    do {
+        going = start;
+        (start = start->next);
+        delete going;
+    } while (--i >= 0);
+}
+void get_splited(string& a1, string& a2, string find) {
 
-long QTIndex(bool qt2,char i) {
-    const char* ptr = strchr(qt2 ? Qt2: Qt, i);
-    return ptr?(ptr - (qt2 ? Qt2 : Qt)):-1;
+    const long extra_find = find.find("?extra=");
+    string finds = find.substr(extra_find + 7);
+
+    const long index = finds.find("#");
+    a1 = finds.substr(0, index);
+    a2 = finds.substr(index + 1);
 }
 
-string Ytv(string e) {
-    std::reverse(e.begin(), e.end());
-    return e;
-}
 
-string Ytr(string e1, long t) {
-    long n;
-    const long size = e1.length();
-    std:vector<char> i = std::vector<char>(e1.length());
-    strcpy(i.data(), e1.c_str());
-    for (long a = size; a--; )
-        if (~(n = QTIndex(true,i[a])))
-            (i[a] = Qt2[n - t]);
-    string return_ = string(i.data());
-    return return_;
+void next_step_t(const long a, const long n, long& t) {
+    t = (n * (a + 1) ^ t + a) % n;
 }
-
-long* function_i(string e, long t) {
+long* function_i(const string e, long t) {
     long n = e.length();
     long* i = new long[(long long)n + 1];
     if (n) {
         long a = n;
         for (t = abs(t); a--; ) {
-            t = (n * (a + 1) ^ t + a) % n;
+            next_step_t(a, n, t);
+            //t = (n * (a + 1) ^ t + a) % n;
             i[a] = t;
         }
     }
     return i;
 }
-string Yts(string e1, long t) {
+
+#pragma endregion
+
+string Ytv(string e) {
+    std::reverse(e.begin(), e.end());
+    return e;
+}
+string Ytr(string e1, long t) {
+    long n;
+    const long size = e1.length();
+    char* i = const_cast<char*>(e1.c_str());
+    for (long a = size; a--; )
+        if (~(n = QTIndex(i[a])))
+            (i[a] = Qt2[n - t]);
+    return e1;
+}
+string Yts(string e1, const long t) {
     const long n = e1.length();
     if (n) {
         long* i = function_i(e1, t);
-        char* e = new char[(long long)n + 1];
-        strcpy(e, e1.data());
+        char* e = const_cast<char*>(e1.c_str());
         for (long a = 0;++a < n; ) {
             //swap e[] items 
             char item = e[a];
@@ -79,186 +110,126 @@ string Yts(string e1, long t) {
             e[i[n - 1 - a]] = item;
         }
         delete[]i;
-        e1 = string(e);
-        delete[]e;
     }
     return e1;
 }
-
-string Yti(string e, long t) {
+string Yti(string e, const long t, const long id) {
     return Yts(e, t ^ id);
 }
-void get_splited(string* a, string find) {
+List* get_splited_from_char_to_array(const string e, long& length, const char sym) {
+    List* start = new List();
+    List* going = start;
 
-    const long extra_find = find.find("?extra=");
-    string finds = find.substr(extra_find + 7);
-
-    const long index = finds.find("#");
-    a[0] = finds.substr(0, index);
-    a[1] = finds.substr(index + 1);
-}
-std::vector<string>* get_splited_from_char_to_array(string e, long& length, char sym) {
-    std::vector<string> *answer = new vector<string> { "" };
+    List* next;
     length = 1;
-    for (long i = 0;i < e.length();i++) {
+    for (unsigned long i = 0;i < e.length();i++) {
+        //next = &A();
         char it = e.at(i);
         if (it == sym) {
-            answer->insert(answer->end(), "");
+            next = new List();
+            going->next = next;
+            going = next;
             length++;
             continue;
         }
-        answer->at(length - 1) += it;
-    }
-    return answer;
-}
+        going->value << it;
 
-string Kt(string e) {
+    }
+    return going;
+}
+string Kt(const string e) {
     if (e.empty() || e.length() % 4 == 1)
         return "";
     stringstream answer;
     char n;
-    long t, n_int, i = 0, a = 0;
-    for (; a < e.length() && (n = e.at(a++)); )
-        if((n_int = QTIndex(false, n)) != -1){
-            if(t = i % 4 ? 64 * t + n_int : n_int, i++ % 4)
+    long t, n_int, i = 0;
+    for (unsigned long a = 0; a < e.length() && (n = e.at(a++)); )
+        if ((n_int = QTIndex(n)) != -1) {
+            t = i % 4 ? 64 * t + n_int : n_int;
+            i++;
+            if (i % 4 != 1)
                 answer << (char)(255 & t >> (-2 * i & 6));
         }
     return answer.str();
 }
-
-string Gt(string e) {
-    if (~e.find("audio_api_unavailable")) {
-
-        string i[2];
-
-        string n[2];
-
-        get_splited(i, e);
-
-        if (i[1] != "") i[1] = Kt(i[1]);
-
-        if (i[0] = Kt(i[0]), i[1].empty() || i[0].empty()) return e;
-
+string Gt(string& e, const long user_id_local) {
+    if (e.find("audio_api_unavailable") != -1) {
+        string i0_kt, i1, n0;
+        get_splited(i0_kt, i1, e);
+        if (i1 != "") i1 = Kt(i1);
+        if (i0_kt = Kt(i0_kt), i1.empty() || i0_kt.empty()) return e;
         long o;
-
         //delete after using
 
-        vector<string> *a = get_splited_from_char_to_array(i[1], o, 9);
-
-        for (;o--;) {
-            string ch = a->at(o);
+        List* start = get_splited_from_char_to_array(i1, o, 9);//o = o ? o.split(String.fromCharCode(9)
+        while (o--) {//o - start length
+            stringstream n0_b, n1_b;
+            string ch = start->value.str();
+            //splice function
             bool b = false;
-            for (long _ = 0;_ < ch.length();_++) {
+            for (unsigned long _ = 0;_ < ch.length();_++) {
                 char item = ch.at(_);
                 if (item == (char)11) {
                     b = true;
                     continue;
                 }
-                n[b] += ch.at(_);
+                if (b) n1_b << ch.at(_);
+                else n0_b << ch.at(_);
             }
-            char t = n[0].at(0);
-            n[0] = i[0];
-            switch (t)
+            //end splice function
+            switch (n0_b.str().at(0))
             {
-                case 'i':i[0] = Yti(n[0], std::stol(n[1]));break;
-                case 's':i[0] = Yts(n[0], std::stol(n[1]));break;
-                case 'v':i[0] = Yts(n[0], std::stol(n[1]));break;
-                case 'r':i[0] = Ytr(n[0], std::stol(n[1]));break;
-                default:
-                    delete a;
-                    return e;
-
+            case 'i':i0_kt = Yti(i0_kt, stol(n1_b.str()), user_id_local);break;
+            case 's':i0_kt = Yts(i0_kt, stol(n1_b.str()));break;
+            case 'v':i0_kt = Ytv(i0_kt);break;
+            case 'r':i0_kt = Ytr(i0_kt, stol(n1_b.str()));break;
+            default:
+                delete_List(start, o);
+                return e;
             }
-
+            List* next = start;
+            start = start->next;
+            delete next;
         }
-        if (i[0] != "" && "http" == i[0].substr(0, 4)) {
-            delete a;
-            return i[0];
+        if (i0_kt != "" && "http" == i0_kt.substr(0, 4)) {
+            return i0_kt;
         }
-        delete a;
     }
+
     return e;
 }
 
 void m3u8ToMp3(string& url) {
-
-    bool b = false;
-
     long m3u8 = url.find("/index.m3u8");
-
     if (m3u8 != -1) {
-
         url = url.replace(m3u8, 11, ".mp3");
-
-        for (long to = -1, i = m3u8;i >= 0;i--) {
-
+        for (int to = -1, i = m3u8;i >= 0;i--) {
             if (url.at(i) == '/') {
+                int delta = to - i;
 
-                if (to == -1)
-
+                if (to == -1 || delta < 10)
                     to = i;
-
                 else {
-
                     url.erase(i + 1, to - i);
-
                     break;
-
                 }
-
             }
-
         }
-
     }
-
 }
-
-#ifdef _DEBUG
-
-int main(int argc, char* argv[]) {
-    string url;
-    if (argc != 3) {
-        //getting url and id
-        cout << "Please enter url to decode:\n";
-        cin >> url;
-
-        cout << "Please enter user-id: ";
-        cin >> id;
-    }
-    else {
-        url = argv[1];
-        try {
-            id = stoi(__argv[2]);
-        }catch (std::invalid_argument ex) {
-            cout << "Second parameter have to be a number";
-            exit(-1);
-        }
-        catch (std::out_of_range ex) {
-            cout << "Second parameter have to be a number";
-            exit(-1);
-        }
-    }
-    string answer = Gt(url);
-    m3u8ToMp3(answer);
-
-    cout << answer;
-
-    return 0;
-}
-
-#else
-EXPORT const char* decode(const char* url, const bool need_mp3 = true) {
+EXPORT char* decode(char* url, const long user_id, const bool need_mp3) {
     string answer;
     try {
         answer = string(url);
-        answer = Gt(answer);
+        answer = Gt(answer, user_id);
+        if (need_mp3)m3u8ToMp3(answer);
+
+        char* z = new char[answer.length() + 1];
+        strcpy(z, answer.c_str());
+        return z;
     }
-    catch (exception e) {}
-    if (need_mp3)m3u8ToMp3(answer);
-    char* e = new char[answer.length()];
-    strcpy(e, answer.c_str());
-    return e;
+    catch (exception e) { return new char[0]; }
+
 }
 EXPORT void setUserId(long i) {
     id = i;
@@ -269,12 +240,9 @@ EXPORT char* toMP3(const char* url) {
     //strcpy( answ,ans.data());
     //return answ;
     m3u8ToMp3(ans);
-    char* e = new char[ans.length()];
+    char* e = new char[ans.length() + 1];
     strcpy(e, ans.c_str());
     return e;
 }
 
-#endif
-
 //compile - g++ -shared -fPIC vk-audio-url-decoder.cpp -o lib.so -rdynamic 
-
